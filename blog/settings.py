8fc2d112cd9eb99a2e4fd4226ccff6b98e0eaf46
Mubi <husseinmubarak500@gmail.com>
2022-04-13 06:34:17 +0100

    changed database to postgres

--- blog/settings.py
+++ blog/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = "blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'codewithmubi',
+        'USER': 'postgres',
+        'PASSWORD': 'mubi',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
